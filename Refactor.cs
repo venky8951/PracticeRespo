c6c681d72b9be3c726832b65a335bbe6688a726d
venky8951 <37590429+venky8951@users.noreply.github.com>
2019-08-26 23:11:04 +0530

    Add files via upload

---
+++ Refactor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//FIND THE CODE SMELL and REFACTOR 
+public interface IPayments
+{
+    void process();
+}
+public class CreditCard : IPayments
+{
+    public void process()
+    {
+        //Credit Card Processing Logic
+    }
+}
+public class PayPal : IPayments
+{
+    public void process()
+    {
+        //Paypal payment Processing Logic
+    }
+}
+public class Google : IPayments
+{
+    public void process()
+    {
+        //Google payment Processing Logic
+    }
+}
+
+public class Amazon : IPayments
+{
+    public void process()
+    {
+        //Amazon payment Processing Logic 
+    }
+}
+public class OnlineCart
+{
+    private IPayments payments;
+    public OnlineCart(IPayments payment)
+    {
+        this.payments = payment;
+    }
+    public void CheckOut()
+    {
+        payments.process();
+    }
+   
+}
